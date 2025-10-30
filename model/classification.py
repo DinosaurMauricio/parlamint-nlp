120ffd9a785ce8eb695b7e6048be58b02f6a6db4
Mauricio <mauricio.eduardo.nr@gmail.com>
2025-10-30 14:13:38 +0100

    unfreeze last two layers from encoder

--- model/classification.py
+++ model/classification.py
@@ -12,6 +12,10 @@ class ClassificationParlamint(nn.Module):
         for param in self.encoder.parameters():
             param.requires_grad = False
 
+        # Unfreeze last two layers
+        for param in self.encoder.encoder.layer[-2:].parameters():
+            param.requires_grad = True
+
         hidden_size = self.encoder.config.hidden_size  # 768
 
         self.dropout = nn.Dropout(dropout)
@@ -26,7 +30,7 @@ class ClassificationParlamint(nn.Module):
     def forward(self, input_ids, attention_mask):
         outputs = self.encoder(input_ids=input_ids, attention_mask=attention_mask)
 
-        pooled_output = outputs.pooler_output  # Use [CLS]
+        pooled_output = outputs.last_hidden_state[:, 0, :]  # Use [CLS]
 
         pooled_output = self.dropout(pooled_output)
         logits = self.classifier(pooled_output)
